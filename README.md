db51d0dd168a62870fb68f6fa4874bc46a983fe6
Zippy6666 <simperzippy@gmail.com>
2024-09-02 19:26:52 +0200

    SubmarineSystem class

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Submarine Assignment
 An assigment regarding effective python code.
+
+# NOTES
+## Teknisk bakgrund och val
+### SubmarineSystem klassen
+- Själva user interfacet, man skapar ett submarine system objekt som man sedan interagerar med som användare
+
+### _Submarine klassen
+- Använder property och setter dekoratorerna
+- Möjliggör så att när man ändrar serienummret på ubåten så kan man kolla innan om serienummret är tillåtet.
+- Är en privat del av SubmarineSystem, man får inte instansiera submarines manuellt!
+
+## Avgränsningar och fokus
+- Fokuset är att göra en väl skriven OOP kod, inte att det ska vara ett realistiskt system för ubåtar.
+
+## Källhänvisningar
+- Inga just nu.
--- submarine_system.py
+++ submarine_system.py
@@ -1,47 +1,65 @@
 from typing import NewType
+from collections import defaultdict
 import re, os
 
 
-SerialNumber = NewType('SerialNumber', str)
+SerialNumber = NewType("SerialNumber", str)
 Position = NewType("Position", list)
 
 
-class Submarine:
-    _serial_number_pattern = re.compile(r"^\d{8}-\d{2}$")
+class SubmarineSystem:
+    serial_number_pattern = re.compile(r"^\d{8}-\d{2}$")
 
-    def __init__(self, serial_number: SerialNumber) -> None:
-        """ A submarine. """
-        self._serial_number: SerialNumber = serial_number
-        self._position: Position = [0, 0]
-        self._sensors: str = "1"*208
+    class _Submarine:
+        def __init__(self, serial_number: SerialNumber) -> None:
+            """A submarine."""
+            self._serial_number: SerialNumber = serial_number
+            self._position: Position = [0, 0]
+            self._sensors: str = "1" * 208
 
-    @property
-    def serial_number(self) -> str:
-        return self._serial_number
+        @property
+        def serial_number(self) -> str:
+            return self._serial_number
 
-    @serial_number.setter
-    def serial_number(self, value: str) -> None:
-        if self._serial_number_pattern.match(value):
+        @serial_number.setter
+        def serial_number(self, value: str) -> None:
             self._serial_number = value
-        else:
+
+    def __init__(self) -> None:
+        """A system for handling submarines"""
+        self._submarines: defaultdict[SerialNumber, self.Submarine] = defaultdict(bool)
+        self._make_dirs()
+
+    def __repr__(self) -> str:
+        return str( [f"Submarine {k} at position {v._position}" for k, v in self._submarines.items()] )
+
+    def _make_dirs(self) -> None:
+        """Make directories if not present."""
+        if not os.path.isdir("MovementReports"):
+            os.mkdir("MovementReports")
+        if not os.path.isdir("SensorData"):
+            os.mkdir("SensorData")
+        if not os.path.isdir("Secrets"):
+            os.mkdir("Secrets")
+
+    def get_submarine(self, serial_number: SerialNumber) -> _Submarine | bool:
+        """Return the submarine instance of the given serialnumber. Returns False if none was found."""
+        return self._submarines[serial_number]
+
+    def register_submarine(self, serial_number: SerialNumber) -> None:
+        if not self.serial_number_pattern.match(serial_number):
             raise ValueError("Serial number must be in the format XXXXXXXX-XX")
+        submarine: self._Submarine = self._Submarine(serial_number)
+        self._submarines[serial_number] = submarine
 
-
-def make_dirs() -> None:
-    """ Make directories if not present """
-    if not os.path.isdir("MovementReports"):
-        os.mkdir("MovementReports")
-    if not os.path.isdir("SensorData"):
-        os.mkdir("SensorData")
-    if not os.path.isdir("Secrets"):
-        os.mkdir("Secrets")
 
 def main() -> None:
-    make_dirs()
-    submarine = Submarine(SerialNumber("12345678-90"))
-    print(submarine.serial_number)
+    submarine_system: SubmarineSystem = SubmarineSystem()
+    submarine_system.register_submarine("11111111-11")
+    submarine_system.register_submarine("22222222-22")
+    print(submarine_system)
+
 
 
 if __name__ == "__main__":
     main()
-    
