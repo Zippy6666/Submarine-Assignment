48dc2f3bf438fb825be989575afa44f1c34ff930
Zippy6666 <simperzippy@gmail.com>
2024-09-07 14:31:41 +0200

    progress

--- submarine_system.py
+++ submarine_system.py
@@ -1,183 +1,151 @@
+from collections.abc import Generator
 from typing import NewType, Optional
-import re, os, time, random, shutil
+from pathlib import Path
+import re, os
 
 
 SerialNumber = NewType("SerialNumber", str)
 Position = NewType("Position", list[int])
-SensorData = NewType("SensorData", list[str])
-
-
-class Submarine:
-    def __init__(
-        self, system: "SubmarineSystem", serial_number: SerialNumber) -> None:
-        """A submarine."""
-        self._serial_number: SerialNumber = serial_number
-        self._position: Position = Position([0, 0])
-        self._sensors: SensorData = ["1" for _ in range(208)]
-        self._system: SubmarineSystem = system
-
-    @property
-    def serial_number(self) -> SerialNumber:
-        return self._serial_number
-
-    @property
-    def sensors(self) -> SensorData:
-        return self._sensors
-
-    @property
-    def position(self) -> Position:
-        return self._position
-
-    @property
-    def dist_from_base(self) -> float:
-        return (self._position[0] ** 2 + self._position[1] ** 2) ** 0.5
-
-    def use_sensors(self) -> None:
-        self._system._log_submarine_sensors(self)
-
-    def move(self, direction: str, distance: int) -> None:
-        match direction:
-            case "up":
-                self._position[0] += distance
-            case "down":
-                self._position[0] -= distance
-            case "left":
-                self._position[1] -= distance
-            case "right":
-                self._position[1] += distance
-            case _:
-                raise ValueError("Not a valid direction.")
-        self._system._log_submarine_movement(
-            SerialNumber(self._serial_number), direction, distance
-        )
-
-    def __repr__(self) -> str:
-        return f"|Submarine {self._serial_number} at {self._position}>|"
+SubmarineReport = NewType("SubmarineReport", str)
 
 
 class SubmarineSystem:
     serial_number_pattern = re.compile(r"^\d{8}-\d{2}$")
 
     def __init__(self) -> None:
-        """The system for handling submarines"""
-        self._submarines: dict[SerialNumber, Submarine] = {}
+        """The system for handling submarines."""
+        self._submarines: dict[SerialNumber, self._Submarine] = {}
+        self._movement_logs: list = []
 
-    def lookup_submarine(self, serial_number: SerialNumber) -> Optional[Submarine]:
-        """Get a submarine by serial number if it exists"""
-        return self._submarines.get(serial_number)
+    def lookup_submarine(self, serial_number: SerialNumber) -> Optional[SubmarineReport]:
+        """Get a submarine by serial number if it exists."""
+        return SubmarineReport( str(self._submarines.get(serial_number)) )
 
-    def get_submarines(self) -> list[Submarine]:
-        """Returns a list of all registered submarines"""
-        return self._submarines.values()
-
-    def register_submarine(
-        self, serial_number: SerialNumber
-    ) -> Submarine:
-        """Register or overwrite a submarine of given serial_number"""
+    def register_submarine(self, serial_number: SerialNumber) -> None:
+        """Register a submarine of given 'serial_number'."""
         if not self.serial_number_pattern.match(serial_number):
             raise ValueError("Serial number must be in the format XXXXXXXX-XX")
-        submarine: Submarine = Submarine(self, serial_number)
+        
+        if not self._submarines.get(serial_number) is None:
+            print(
+                f"Warning: Submarine {serial_number} already registered! Overwriting."
+            )
+
+        submarine: self._Submarine = self._Submarine(serial_number)
         self._submarines[serial_number] = submarine
-        return submarine
 
-    def get_furthest_submarine(self) -> Submarine:
-        """Get the submarine furthest from the base"""
-        submarines_sorted_by_dist: list[Submarine] = (
+    def register_submarines_by_movement_reports(self) -> Generator[SerialNumber]:
+        """Register submarines by movement reports."""
+        if not os.path.isdir("MovementReports"):
+            raise FileNotFoundError("No 'MovementReports' directory detected.")
+        
+        for file_name in os.listdir("MovementReports"):
+            serial_number: str = Path(file_name).stem
+            if self.serial_number_pattern.match(serial_number):
+                self.register_submarine(serial_number)
+                yield serial_number
+
+    def move_submarine_by_reports(self, serial_number: SerialNumber) -> None:
+        """ Read movement reports for this submarine, and move it accordingly """
+        if not os.path.isfile(f"MovementReports/{serial_number}.txt"):
+            raise FileNotFoundError("No movement reports file detected.")
+        
+        sub: Optional[self._Submarine] = self._submarines.get(serial_number)
+        
+        if not sub:
+            raise Exception(f"Submarine {serial_number} not found.")
+
+        with open(f"MovementReports/{serial_number}.txt") as f:
+            for i, line in enumerate( f ):
+                split: list[str] = line.split()
+
+                # This movement report was invalid, skip and continue
+                if len(split) != 2 or not split[1].isdecimal():
+                    print(f"Warning: Movement report {i} is invalid. Skipping.")
+                    continue
+
+                sub.move(split[0], int(split[1]))
+                
+    def get_furthest_submarine(self) -> SubmarineReport:
+        """Get the submarine furthest from the base."""
+        sorted_subs: list[self._Submarine] = (
             self._submarines_sorted_by_distance()
         )
-        return submarines_sorted_by_dist[len(submarines_sorted_by_dist) - 1]
+        return SubmarineReport( str(sorted_subs[len(sorted_subs) - 1]) )
 
-    def get_closest_submarine(self) -> Submarine:
-        """Get the submarine closest to the base"""
-        submarines_sorted_by_dist: list[Submarine] = (
+    def get_closest_submarine(self) -> SubmarineReport:
+        """Get the submarine closest to the base."""
+        sorted_subs: list[self._Submarine] = (
             self._submarines_sorted_by_distance()
         )
-        return submarines_sorted_by_dist[0]
+        return SubmarineReport( str(sorted_subs[0]) )
 
-    def get_lowest_submarine(self) -> Submarine:
-        """Get the submarine at the lowest point form the base"""
-        submarines_sorted_by_dist: list[Submarine] = (
+    def get_lowest_submarine(self) -> SubmarineReport:
+        """Get the submarine at the lowest point form the base."""
+        sorted_subs: list[self._Submarine] = (
             self._submarines_sorted_by_vertical_distance()
         )
-        return submarines_sorted_by_dist[0]
+        return SubmarineReport( str(sorted_subs[0]) )
 
-    def get_highest_submarine(self) -> Submarine:
-        """Get the submarine at the highest point from the base"""
-        submarines_sorted_by_dist: list[Submarine] = (
+    def get_highest_submarine(self) -> SubmarineReport:
+        """Get the submarine at the highest point from the base."""
+        sorted_subs: list[self._Submarine] = (
             self._submarines_sorted_by_vertical_distance()
         )
-        return submarines_sorted_by_dist[len(submarines_sorted_by_dist) - 1]
+        return SubmarineReport( str(sorted_subs[len(sorted_subs) - 1]) )
 
-    def _log_submarine_movement(
-        self, serial_number: SerialNumber, direction: str, distance: int
-    ) -> None:
-        """Log submarine movement"""
-        # with open("MovementLog/" + serial_number + ".txt", "a") as file:
-        #     file.write(f"{direction} {distance}\n")
-
-    def _submarines_sorted_by_distance(self) -> list[Submarine]:
-        """Get submarines sorted by distance from the base"""
+    def _submarines_sorted_by_distance(self) -> list["_Submarine"]:
+        """Get submarines sorted by distance from the base."""
         return sorted(
             self._submarines.values(), key=lambda submarine: submarine.dist_from_base
         )
 
-    def _submarines_sorted_by_vertical_distance(self) -> list[Submarine]:
-        """Get submarines sorted by altetude from the base"""
+    def _submarines_sorted_by_vertical_distance(self) -> list["_Submarine"]:
+        """Get submarines sorted by altetude from the base."""
         return sorted(
             self._submarines.values(), key=lambda submarine: submarine.position[0]
         )
 
+    class _Submarine:
+        def __init__(self, serial_number: SerialNumber) -> None:
+            """A submarine."""
+            self._serial_number: SerialNumber = serial_number
+            self._position: Position = Position([0, 0])
 
-# def simulate(system: SubmarineSystem) -> None:
-#     """Simulates the movements and sensors of the submarines"""
-#     for sub in system.get_submarines():
-#         # Move submarines randomly
-#         if random.randint(1, 2) == 1:
-#             sub.move(
-#                 random.choice(("up", "down", "left", "right")), random.randint(1, 10)
-#             )
+        @property
+        def serial_number(self) -> SerialNumber:
+            return self._serial_number
 
-#         # Break sensors randomly
-#         if random.randint(1, 4) == 1:
-#             sub.sensors[random.randint(0, 207)] = "0"
+        @property
+        def position(self) -> Position:
+            return self._position
 
-#         # Use sensors, this will log sensor data
-#         sub.use_sensors()
+        @property
+        def dist_from_base(self) -> float:
+            return (self._position[0] ** 2 + self._position[1] ** 2) ** 0.5
+
+        def move(self, dir:str, dist:int) -> None:
+            match dir:
+                case "up":
+                    self._position[0] += dist
+                case "down":
+                    self._position[0] -= dist
+                case "forward":
+                    self._position[1] += dist
+                case _:
+                    print(f"Warning: Invalid direction '{dir}'")
+
+        def __str__(self) -> str:
+            return f"|Submarine {self._serial_number} at {self._position}>|"
 
 
 def main() -> None:
-    ...
-    # """Submarine simulation"""
+    system: SubmarineSystem = SubmarineSystem()
 
-    # # Remove old dirs / make new ones
-    # shutil.rmtree("MovementLog", ignore_errors=True)
-    # shutil.rmtree("SensorData", ignore_errors=True)
-    # shutil.rmtree("Secrets", ignore_errors=True)
-    # os.makedirs("MovementLog", exist_ok=True)
-    # os.makedirs("SensorData", exist_ok=True)
-    # os.makedirs("Secrets", exist_ok=True)
-
-    # system: SubmarineSystem = SubmarineSystem()
-
-    # # Register some example submarines
-    # print("Generating example submarines...")
-    # submarine_count: int = 3000
-    # for i in range(submarine_count):
-    #     serial_number: SerialNumber = SerialNumber(f"1234{i:04}-42")
-    #     system.register_submarine(serial_number)
-    # print(submarine_count, "submarines created!")
-
-    # print("Simulating submarines...")
-    # while True:
-    #     simulate(system)
-    #     time.sleep(5)
-    #     sub_closest = system.get_closest_submarine()
-    #     sub_furthest = system.get_furthest_submarine()
-    #     sub_highest = system.get_highest_submarine()
-    #     sub_lowest = system.get_lowest_submarine()
-    #     print(
-    #         f"One iteration done, closest {sub_closest}, furthest {sub_furthest}, highest {sub_highest}, lowest {sub_lowest}"
-    #     )
+    for serial_number in system.register_submarines_by_movement_reports(): # ~ 6000 files
+        system.move_submarine_by_reports(serial_number) # ~ 100_000 line file read...
+        report: SubmarineReport = system.lookup_submarine(serial_number)
+        print(f"Movement reports fetched for {report}")
 
 
 if __name__ == "__main__":
